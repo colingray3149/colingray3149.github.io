006d8067c059ab5cca93b686346e1cc11e1e9fa4
Colin Gray <gray.d.colin@gmail.com>
2025-08-30 18:41:23 -0400

    upload final everything

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+    import { defineConfig } from 'astro/config';
+
+    // https://astro.build/config
+    export default defineConfig({
+      site: 'https://colingray3149.github.io', // This is the crucial line
+    });
